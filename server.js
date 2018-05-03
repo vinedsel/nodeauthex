a12c06957f96910ec12cc144b5929e0220e291f3
Raymond Gregory <vinedsel@gmail.com>
2018-05-03 16:57:17 -0500

    Uncommmented require passport line.

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ module.exports = {
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
